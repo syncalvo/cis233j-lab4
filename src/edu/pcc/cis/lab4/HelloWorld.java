9c3212523fb724bded32d08daf7d853404ba8d4c
Syn Calvo <syn@syncalvo.com>
2019-02-04 21:57:39 -0800

    added print date to the main method

--- src/edu/pcc/cis/lab4/HelloWorld.java
+++ src/edu/pcc/cis/lab4/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.lab4;
 
+import java.util.Date;
+
 /**
  * Author: Syn Calvo
  * Version: 2019.02.04
@@ -9,5 +11,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
