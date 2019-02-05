b781a7e2cc4d1a1ae8e4f7eb0134ac6f832f0c4d
Syn Calvo <syn@syncalvo.com>
2019-02-04 21:55:41 -0800

    added main method that prints hello wolrd

--- src/edu/pcc/cis/lab4/HelloWorld.java
+++ src/edu/pcc/cis/lab4/HelloWorld.java
@@ -1,4 +1,13 @@
 package edu.pcc.cis.lab4;
 
+/**
+ * Author: Syn Calvo
+ * Version: 2019.02.04
+ */
+
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
 }
